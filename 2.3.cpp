f30f886a07d85f8d6c1ad50a2abb3a744728e9a6
Aleksei <87711803+kybik0606@users.noreply.github.com>
2023-11-10 17:42:05 +0300

    my first task

---
+++ 2.3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int a, b, nod;
+    double nok;
+    cout << "Введите первое число:";
+    cin >> a;
+    cout << "Введите второе число:";
+    cin >> b;
+
+    for (int i = 1; i <= a; i++) {
+        if (a % i == 0 && b % i == 0)
+            nod = i;
+    }
+    nok = a * b / nod;
+    cout << "НОK: " << nok;
+}
